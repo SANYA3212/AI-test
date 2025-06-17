306b42be87397f45a4f3407d104cbf8cac441577
SANYA <147943624+SANYA3212@users.noreply.github.com>
2025-06-17 16:09:11 +0600

    Add files via upload

--- install_dependencies.bat
+++ install_dependencies.bat
@@ -1,6 +1,21 @@
 @echo off
-python -m venv venv
-call venv\Scripts\activate.bat
-pip install -r requirements.txt
-pip install psutil
+echo Установка всех зависимостей для Ollama + LangChain + Tools...
+
+REM Основные библиотеки LangChain
+pip install -U langchain langchain-community langchain-experimental langchain-ollama
+
+REM Поисковик
+pip install -U duckduckgo-search
+
+REM Работа с PDF
+pip install -U PyPDF2
+
+REM Голосовой ввод
+pip install -U SpeechRecognition
+
+REM Озвучка (TTS)
+pip install -U pyttsx3
+
+pip install WMI
+echo Установка завершена!
 pause
